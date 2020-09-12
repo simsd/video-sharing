c948afe3e78b836d0afda23262a91e78031ec1aa
Simran Singh Dhaliwal <sdhaliwal@ucla.edu>
2020-09-12 12:19:28 -0700

    Worked on front-end javascript
    Can read in user-input

--- client/client.js
+++ client/client.js
@@ -1,3 +1,23 @@
 
 
 const API_URL = "http://localhost:5000/videos";
+
+
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault(); //do not refresh the page
+    const formData = new FormData(form);
+
+    const userName = formData.get('username');
+    const videoLink = formData.get('videolink');
+    
+    const vid = {
+        userName, 
+        videoLink
+    };
+
+    console.log(vid);
+
+});
